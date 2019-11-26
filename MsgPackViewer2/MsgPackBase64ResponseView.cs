163cdcfc5da641d767f2010f17354b6120631ebf
kagasu <admin@kagasu.net>
2019-11-26 15:15:05 +0900

    fix

---
+++ MsgPackViewer2/MsgPackBase64ResponseView.cs
@@ -0,0 +1,14 @@
+﻿using Fiddler;
+
+namespace MsgPackViewer2
+{
+    public class MsgPackBase64ResponseView : MsgPackViewBaseBase64, IResponseInspector2
+    {
+        public HTTPResponseHeaders headers { get; set; }
+
+        protected override HTTPHeaders GetHeaders()
+        {
+            return headers;
+        }
+    }
+}
